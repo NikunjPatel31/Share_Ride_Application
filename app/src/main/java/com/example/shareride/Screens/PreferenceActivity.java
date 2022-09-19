e1ceee3d77ab564055a23788cb6460f0a886339f
krupa1420 <90409587+krupa1420@users.noreply.github.com>
2022-09-19 17:29:37 +0530

    recyclerview

--- app/src/main/java/com/example/shareride/Screens/PreferenceActivity.java
+++ app/src/main/java/com/example/shareride/Screens/PreferenceActivity.java
@@ -1,6 +1,7 @@
 package com.example.shareride.Screens;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -8,7 +9,10 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.shareride.R;
+import com.example.shareride.RecyclerViewAdapter.PreferencesRecyclerViewAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class PreferenceActivity extends AppCompatActivity {
 
@@ -16,25 +20,42 @@ public class PreferenceActivity extends AppCompatActivity {
     FloatingActionButton btnnsmoker,btnnbabies,btnadult,btnomen,btnowomen,btnnpet;
     RecyclerView recyclerView;
 
+    // local instances
+    ArrayList<String> list = new ArrayList<>();
     Boolean isSmoker=false;
+    PreferencesRecyclerViewAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preference);
         getWindow().setBackgroundDrawableResource(R.drawable.background);
         initializeComponents();
+
+//        list.add("No Smoking");
+//        list.add("No babies");
+//        list.add("No krupa");
+//        list.add("No jeetu");
+
         btnnsmoker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (!isSmoker) {
                     isSmoker = true;
-                    btnnsmoker.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_horizontal_rule_24));
+                    list.add("No smoking");
+                    adapter = new PreferencesRecyclerViewAdapter(list,PreferenceActivity.this);
+                    recyclerView.setAdapter(adapter);
+
+//                    btnnsmoker.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_horizontal_rule_24));
                 } else {
                     isSmoker = false;
                     btnnsmoker.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_add_24));
                 }
             }
         });
+
+        adapter = new PreferencesRecyclerViewAdapter(list,PreferenceActivity.this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.setAdapter(adapter);
     }
 
     private void initializeComponents() {
