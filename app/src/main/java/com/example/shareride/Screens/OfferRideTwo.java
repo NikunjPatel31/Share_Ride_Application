a76b6ecc2face7ad1c70ab2b8134689bc5f259a8
Nikunj Patel <59450797+NikunjPatel31@users.noreply.github.com>
2022-09-21 11:46:26 +0530

    Number of seats can be increased or decreased

--- app/src/main/java/com/example/shareride/Screens/OfferRideTwo.java
+++ app/src/main/java/com/example/shareride/Screens/OfferRideTwo.java
@@ -4,18 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.shareride.R;
 
 public class OfferRideTwo extends AppCompatActivity {
 
+    // components
+    private EditText etCostPerSeat;
+    private TextView tvNumberOfSeats;
+
     public void offer(View view) {
 
+    }
+
+    public void decreaseNumber(View view) {
+        int value = Integer.parseInt(tvNumberOfSeats.getText().toString());
+        if (value > 0) {
+            tvNumberOfSeats.setText(""+(--value));
+        }
+    }
+
+    public void increaseNumber(View view) {
+        int value = Integer.parseInt(tvNumberOfSeats.getText().toString());
+        tvNumberOfSeats.setText(""+(++value));
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offer_ride_two);
+        initializeComponents();
+    }
+
+    private void initializeComponents() {
+        etCostPerSeat = findViewById(R.id.cost_per_seat_edittext);
+        tvNumberOfSeats = findViewById(R.id.number_of_seats_textview);
     }
 }
