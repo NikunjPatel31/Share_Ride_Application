6190f700d3bc2001e4419a52f003bda57ac9a7d9
PrachiVasava <Prachivasava30@gmail.com>
2022-09-26 17:13:34 +0530

    prachi

---
+++ app/src/main/java/com/example/shareride/Screens/RideInformation.java
@@ -0,0 +1,16 @@
+package com.example.shareride.Screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shareride.R;
+
+public class RideInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ride_information);
+    }
+}
